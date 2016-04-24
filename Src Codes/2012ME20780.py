5441074394aa25fffbfc8b0e01b6f90f239c1089
kautsiitd <kautsiitd@gmail.com>
2016-04-25 00:13:20 +0530

    Report added

--- Src Codes/2012ME20780.py
+++ Src Codes/2012ME20780.py
@@ -3,19 +3,22 @@ import random
 from collections import Counter
 import math
 
-def generate_samples():
-	for i in range(1000):
-		print random.gauss(0,2),"\t0"
-	for i in range(100):
-		print random.gauss(5,1), "\t1"
+def generate_samples(filename):
+	with open(filename,"wb") as f:
+		for i in range(1000):
+			f.write(str(random.gauss(0,2)))
+			f.write("\t0\n")
+		for i in range(1000):
+			f.write(str(random.gauss(5,1)))
+			f.write("\t1\n")
 
-def get_density(x, m, s):
-	return (1/(s* math.sqrt(2 * math.pi))) * (math.pow(math.e, -(x-m) * (x-m) / (2 * s * s)))
+def get_density(point, mean, stdevs):
+	return (1/(stdevs* math.sqrt(2 * math.pi))) * (math.pow(math.e, -(point-mean)*(point-mean) / (2*stdevs*stdevs)))
 
-def get_posterior(x, priors, means, stdevs):
+def get_posterior(point, priors, means, stdevs):
 	posteriors = Counter()
 	for k in priors.keys():
-		posteriors[k] = get_density(x, means[k], stdevs[k]) * priors[k]
+		posteriors[k] = get_density(point, means[k], stdevs[k]) * priors[k]
 
 	sum_posteriors = sum(posteriors.values())
 	for k in posteriors.keys():
@@ -45,25 +48,28 @@ def relearn(plines):
 
 def main():
 	op = sys.argv[1]
+	trainfile = "../Output/gmm/gmm.test"
+	outputfile= "../Output/gmm/gmm.output"
+	f = open(outputfile,"wb")
 	if op == "generate":
-		generate_samples()
+		generate_samples(trainfile)
 	else:
-		filename = "../Output/gmm/gmm.test"
 		priors = Counter({"0": 0.5, "1":0.5})
 		means  = Counter({"0": random.random(), "1":random.random()})
 		stdevs = Counter({"0": random.random(), "1":random.random()})
 
 		for i in range(1000):
-			lines = [l.strip().split() for l in open(filename).readlines()]
+			lines = [l.strip().split() for l in open(trainfile).readlines()]
 			plines= []
 			for line in lines:
 				plines.append((get_posterior(float(line[0]), priors, means, stdevs), line))
 
-			print "Iterations:", i
-			print "Priors: ", priors
-			print "Means: " , means
-			print "Stdevs: ", stdevs
+			f.write("Iterations: "+str(i)+"\n")
+			f.write("Priors: "+str(priors)+"\n")
+			f.write("Means: "+str(means)+"\n")
+			f.write("Stdevs: "+str(stdevs)+"\n")
 			(priors, means, stdevs) = relearn(plines)
+	f.close()
 
 if __name__ == '__main__':
 	main()
