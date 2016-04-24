ee438119b30f50b4c6edd232f69c653426bfaf03
kautsiitd <kautsiitd@gmail.com>
2016-04-24 18:42:53 +0530

    files rearranged

---
+++ Src Codes/2012ME20780.py
@@ -0,0 +1,69 @@
+import sys
+import random
+from collections import Counter
+import math
+
+def generate_samples():
+	for i in range(1000):
+		print random.gauss(0,2),"\t0"
+	for i in range(100):
+		print random.gauss(5,1), "\t1"
+
+def get_density(x, m, s):
+	return (1/(s* math.sqrt(2 * math.pi))) * (math.pow(math.e, -(x-m) * (x-m) / (2 * s * s)))
+
+def get_posterior(x, priors, means, stdevs):
+	posteriors = Counter()
+	for k in priors.keys():
+		posteriors[k] = get_density(x, means[k], stdevs[k]) * priors[k]
+
+	sum_posteriors = sum(posteriors.values())
+	for k in posteriors.keys():
+		posteriors[k] /= sum_posteriors
+	
+	return posteriors
+
+def relearn(plines):
+	priors = Counter()
+	means  = Counter()
+	stdevs = Counter()
+
+	for (posterior, line) in plines:
+		for k in posterior.keys():
+			priors[k] += posterior[k]
+			means[k]  += posterior[k] * float(line[0])
+			stdevs[k] += posterior[k] * float(line[0]) * float(line[0])
+
+		sum_priors = sum(priors.values())
+		for k in priors.keys():
+			means[k] /= priors[k]
+			stdevs[k]/= priors[k]
+			stdevs[k] = math.sqrt(stdevs[k] - means[k] * means[k])
+			priors[k]/= sum_priors
+
+		return (priors, means, stdevs)
+
+def main():
+	op = sys.argv[1]
+	if op == "generate":
+		generate_samples()
+	else:
+		filename = "../Output/gmm/gmm.test"
+		priors = Counter({"0": 0.5, "1":0.5})
+		means  = Counter({"0": random.random(), "1":random.random()})
+		stdevs = Counter({"0": random.random(), "1":random.random()})
+
+		for i in range(1000):
+			lines = [l.strip().split() for l in open(filename).readlines()]
+			plines= []
+			for line in lines:
+				plines.append((get_posterior(float(line[0]), priors, means, stdevs), line))
+
+			print "Iterations:", i
+			print "Priors: ", priors
+			print "Means: " , means
+			print "Stdevs: ", stdevs
+			(priors, means, stdevs) = relearn(plines)
+
+if __name__ == '__main__':
+	main()
